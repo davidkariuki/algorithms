e4c51de989a4ef5e3ca9f49eb589df736bc5cec7
David Kariukí <dkariuki@gmail.com>
2014-04-24 00:50:02 -0700

    rename some variables to match blog code

--- lib/algorithms/kth_smallest.rb
+++ lib/algorithms/kth_smallest.rb
@@ -8,15 +8,15 @@ module Algorithms
       array.each_slice(5) do |array_5|
         medians << select(array_5, array_5.length/2)
       end
-      pivot = kth_smallest(medians, medians.length/2)
-      array.each { |n| n < pivot ? left << n : (n > pivot ? right << n : nil) }
-      pivot_rank = left.length + 1
-      if k == pivot_rank
-        pivot
-      elsif k < pivot_rank
+      m = kth_smallest(medians, medians.length/2)
+      array.each { |n| n < m ? left << n : (n > m ? right << n : nil) }
+      m_posn = left.length + 1
+      if k == m_posn
+        m
+      elsif k < m_posn
         kth_smallest(left, k)
       else
-        kth_smallest(right, k - pivot_rank)
+        kth_smallest(right, k - m_posn)
       end
     end
 
